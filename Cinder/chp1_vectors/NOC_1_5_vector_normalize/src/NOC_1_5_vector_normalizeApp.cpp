3aa4443c72063ab8d1b2010da0ef575497aed54f
Greg Kepler <gkepler@barbariangroup.com>
2012-11-11 07:39:15 -0500

    Added NOC_1_5_vector_normalize

---
+++ Cinder/chp1_vectors/NOC_1_5_vector_normalize/src/NOC_1_5_vector_normalizeApp.cpp
@@ -0,0 +1,66 @@
+//
+//  Example 1-5: Normalize
+//  Demonstration of normalizing a vector.
+//  Normalizing a vector sets its length to 1.
+//  The Nature of Code
+//
+//  Converted from Daniel Shiffman's Processing Examples
+//  Created by Greg Kepler
+//
+
+
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class NOC_1_5_vector_normalizeApp : public AppBasic {
+  public:
+	void prepareSettings( Settings *settings);
+	void setup();
+	void update();
+	void draw();
+};
+
+void NOC_1_5_vector_normalizeApp::prepareSettings( Settings *settings )
+{
+	settings->setWindowSize( 800, 200 );
+}
+
+void NOC_1_5_vector_normalizeApp::setup()
+{
+}
+
+void NOC_1_5_vector_normalizeApp::update()
+{
+}
+
+void NOC_1_5_vector_normalizeApp::draw()
+{
+	gl::clear( Color( 1, 1, 1 ) );
+	
+	// A vector that points to the mouse location
+	Vec2f mouse = Vec2f( getMousePos().x, getMousePos().y );
+	// A vector that points to the center of the window
+	Vec2f center = Vec2f( getWindowWidth()/2, getWindowHeight()/2 );
+	// Subtract center from mouse which results in a vector that points from center to mouse
+	mouse -= center;
+	
+	// Normalize the vector
+	mouse.normalize();
+	
+	// Multiply its length by 50
+	mouse *= 150;
+	
+	// Need push and pop matrix since the matrix doesn't reset on draw like in processing
+	glPushMatrix();
+	gl::translate( getWindowWidth()/2, getWindowHeight()/2 );
+	glLineWidth( 2.0 );
+	gl::color( 0, 0, 0 );
+	gl::drawLine( Vec2f( 0,0 ), Vec2f( mouse.x, mouse.y ) );
+	glPopMatrix();
+}
+
+CINDER_APP_BASIC( NOC_1_5_vector_normalizeApp, RendererGl )
