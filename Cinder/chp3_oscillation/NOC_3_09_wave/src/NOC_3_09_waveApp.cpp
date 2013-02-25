8fd9226bd2d3c4b4b65a15983b8bd20c379ef407
matthewepler <matthewepler@gmail.com>
2013-02-24 20:51:32 -0500

    3_09_wave

---
+++ Cinder/chp3_oscillation/NOC_3_09_wave/src/NOC_3_09_waveApp.cpp
@@ -0,0 +1,58 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class NOC_3_09_waveApp : public AppBasic {
+  public:
+    void prepareSettings( Settings *settings );
+	void setup();
+	void update();
+	void draw();
+    
+    float startAngle;
+    float angleVel;
+};
+
+void NOC_3_09_waveApp::prepareSettings( Settings *settings )
+{
+    settings->setWindowSize( 800, 200 );
+}
+
+void NOC_3_09_waveApp::setup()
+{
+    gl::enableAlphaBlending();
+    
+    startAngle = 0;
+    angleVel   = 0.23;
+}
+
+void NOC_3_09_waveApp::update()
+{
+}
+
+void NOC_3_09_waveApp::draw()
+{
+	gl::clear( Color( 1, 1, 1 ) );
+
+    startAngle += 0.015;
+    float angle = startAngle;
+    
+    for( int x = 0; x < getWindowWidth(); x += 24 )
+    {
+        float y = lmap<float>( sin( angle ), -1, 1, 0, getWindowHeight() );
+        float alphaVal = lmap<float>( 50, 0, 255, 0, 1 );
+        gl::color( 0, 0, 0, alphaVal );
+        gl::drawSolidEllipse( Vec2f( x, y ), 24, 24 );
+        gl::color( 0, 0, 0 );
+        glLineWidth( 2 );
+        gl::drawStrokedEllipse( Vec2f( x, y ), 24, 24 );
+        
+        angle += angleVel;
+    }
+}
+
+
+CINDER_APP_BASIC( NOC_3_09_waveApp, RendererGl )
